716c4ad07c69ed05b8c95daf1944174680eb4916
Pol de Font-Réaulx <pol.de-font-reaulx@artefact.com>
2022-10-28 10:46:11 +0200

    feat: schema with all redis data structure

--- recommandation_service/schemas.py
+++ recommandation_service/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List
 
 from pydantic import BaseModel
@@ -7,6 +8,10 @@ class Article(BaseModel):
     id: str
     title: str
     abstract: str
+    authors: List[str]
+    categories: List[str]
+    published: datetime
+    updated: datetime
 
 
 class Articles(BaseModel):
